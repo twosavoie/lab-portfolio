88b39cd789e5f8917763af008c7d5b6481fab3ee
lsavoie <twosavoie@gmail.com>
2022-08-27 09:55:49 -0500

    Hover shows static image above section

--- js/main.js
+++ js/main.js
@@ -20,20 +20,57 @@ const navItem = document.querySelectorAll(".nav-item");
 navItem.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    // navMenu.classList.add("hide"); // trying 
-    hamburger.setAttribute('aria-expanded', 'false');
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  // navMenu.classList.add("hide"); // trying 
+  hamburger.setAttribute('aria-expanded', 'false');
 }
 
-hamburger.addEventListener('click', function(){
-    if (hamburger.classList.contains('active')) {
-      this.setAttribute('aria-expanded', 'false');
-      hamburger.classList.remove('active');
-      navMenu.classList.remove("active");
-    } else {
-      hamburger.classList.add('active'); 
-      navMenu.classList.toggle("active");
-      this.setAttribute('aria-expanded', 'true');
+hamburger.addEventListener('click', function () {
+  if (hamburger.classList.contains('active')) {
+    this.setAttribute('aria-expanded', 'false');
+    hamburger.classList.remove('active');
+    navMenu.classList.remove("active");
+  } else {
+    hamburger.classList.add('active');
+    navMenu.classList.toggle("active");
+    this.setAttribute('aria-expanded', 'true');
+  }
+});
+
+// Code for modal
+var image = document.querySelectorAll(".enlarge-image");
+var modalImage = document.querySelector("img .modal-image"); // how to change the modal image???
+var modal = document.querySelector(".modal");
+var modalX = document.querySelector(".modal-x");
+
+var closeModal = function () {
+  modal.classList.remove("show-modal");
+};
+
+var openModal = function () {
+  modal.classList.add("show-modal");
+  modalImage = image.src;
+  console.log(modalImage); //undefined
+};
+
+image.forEach((image) => {
+  console.log(image.src);
+  image.addEventListener("mouseover", function () {
+    openModal();
+  })
+});
+
+modalX.addEventListener("click", function () {
+  closeModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
     }
-  });
+  }
+});
+
